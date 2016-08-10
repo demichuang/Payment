9257a358dffeb95468223bc477ba0345993927b5
hi <here>
2016-08-10 01:05:33 +0000

    修改controller內判斷點選按鈕的寫法

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -17,20 +17,28 @@ class indexController extends Controller
         $num = $this->model("sqlcommand")->checkUser($name);
 
         if ($num == 1) {
-            if (isset($_POST["in"]) && $money > 0) {         // 點選"存款按鈕"
+            if (isset($_POST["in"]) && $money > 0) {   // 點選"存款按鈕"
                 $msg = $this->model("sqlcommand")->moneyDeposit($name, $money);
                 $this->view("index", $msg, "");
-            } elseif (isset($_POST["out"]) && $money > 0) {  // 點選"提款按鈕"
-                $msg = $this->model("sqlcommand")->moneyWithdraw($name, $money);
-                $this->view("index", $msg, "");
-            } elseif (isset($_POST["searchmoney"])) {        // 點選"查詢餘額按鈕"
-                $msg = $this->model("sqlcommand")->moneySearch($name);
-                $this->view("index", $msg, "");
-            } elseif (isset($_POST["searchdetail"])) {       // 點選"查詢明細按鈕"
-                $msg = $this->model("sqlcommand")->detailSearch($name);
-                $this->view("index", $msg[0], $msg[1]);
             } else {
                 $this->view("index", "輸入錯誤", "");
+            }
+
+            if (isset($_POST["out"]) && $money > 0) {  // 點選"提款按鈕"
+                $msg = $this->model("sqlcommand")->moneyWithdraw($name, $money);
+                $this->view("index", $msg, "");
+            } else {
+                $this->view("index", "輸入錯誤", "");
+            }
+
+            if (isset($_POST["searchmoney"])) {        // 點選"查詢餘額按鈕"
+                $msg = $this->model("sqlcommand")->moneySearch($name);
+                $this->view("index", $msg, "");
+            }
+
+            if (isset($_POST["searchdetail"])) {       // 點選"查詢明細按鈕"
+                $msg = $this->model("sqlcommand")->detailSearch($name);
+                $this->view("index", $msg[0], $msg[1]);
             }
         } else {
             $this->view("index", "查無此帳戶", "");
