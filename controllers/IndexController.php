347735a027cbf7efe9df3cb6948b21b42f33d78d
hi <here>
2016-08-10 02:05:39 +0000

    修正Controller的bug

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -14,27 +14,29 @@ class IndexController extends Controller
         $name = $_POST['txtAccountname'];       // 輸入帳戶名
         $money = $_POST['txtMoney'];            // 輸入金額
 
-        $num = $this->model("sqlcommand")->checkUser($name);
+        $num = $this->model("SqlCommand")->checkUser($name);
 
         if ($num == 1) {
-            if (isset($_POST["in"]) && $money > 0) {         // 點選"存款按鈕"
-                $msg = $this->model("sqlcommand")->moneyDeposit($name, $money);
+            if (isset($_POST["in"]) && $money > 0) {    // 點選"存款按鈕"
+                $msg = $this->model("SqlCommand")->moneyDeposit($name, $money);
                 $this->view("index", $msg, "");
-            } elseif (isset($_POST["out"]) && $money > 0) {  // 點選"提款按鈕"
-                $msg = $this->model("sqlcommand")->moneyWithdraw($name, $money);
+            }
+
+            if (isset($_POST["out"]) && $money > 0) {   // 點選"提款按鈕"
+                $msg = $this->model("SqlCommand")->moneyWithdraw($name, $money);
                 $this->view("index", $msg, "");
+            }
+
+            if (isset($_POST["searchmoney"])) {         // 點選"查詢餘額按鈕"
+                $msg = $this->model("SqlCommand")->moneySearch($name);
+                $this->view("index", $msg, "");
+            }
+
+            if (isset($_POST["searchdetail"])) {        // 點選"查詢明細按鈕"
+                $msg = $this->model("SqlCommand")->detailSearch($name);
+                $this->view("index", $msg[0], $msg[1]);
             } else {
                 $this->view("index", "輸入錯誤", "");
-            }
-
-            if (isset($_POST["searchmoney"])) {              // 點選"查詢餘額按鈕"
-                $msg = $this->model("sqlcommand")->moneySearch($name);
-                $this->view("index", $msg, "");
-            }
-
-            if (isset($_POST["searchdetail"])) {             // 點選"查詢明細按鈕"
-                $msg = $this->model("sqlcommand")->detailSearch($name);
-                $this->view("index", $msg[0], $msg[1]);
             }
         } else {
             $this->view("index", "查無此帳戶", "");
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 header('Content-type: text/html; charset=utf-8');   //使用萬用字元碼utf-8
 
-require_once 'models/connect_db.php';
+require_once 'models/Connect.php';
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 
