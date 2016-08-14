d57b791b8382751383baf1fbcebe9a24a4e7734c
hi <demichuang40207@gmail.com>
2016-08-14 03:25:37 +0000

    測試查詢明細ok

--- Test/SqlCommandTest.php
+++ Test/SqlCommandTest.php
@@ -26,20 +26,25 @@ class SqlCommandTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(2000, $money);
     }
 
-    // // 測試查詢明細
-    // public function testDetailSearch()
-    // {
-    // 	$account = "jj";
-    // 	$expect = ['id' => '7',
-    //                 'username' => 'jj',
-    // 	            'action' => '存入',
-    // 	            'money' => '2000',
-    // 	            'balance' => '2000',
-    //                 'time' => '2016-08-02 05:39:28'];
+    // 測試查詢明細
+    public function testDetailSearch()
+    {
+    	$account = "jj";
+    	$expect = array (0 => array ('id' => "7",
+    	                             0 => "7",
+    	                             'username' => "jj",
+    	                             1 => "jj",
+    	                             'action' => "存入",
+    	                             2 => "存入",
+    	                             'money' => "2000",
+    	                             3 => "2000",
+    	                             'balance' => "2000",
+    	                             4 => "2000",
+    	                             'time' => "2016-08-02 05:39:28",
+    	                             5 => "2016-08-02 05:39:28" ));
+        $sql = new SqlCommand();
+        $row = $sql->detailSearch($account);
 
-    //     $sql = new SqlCommand();
-    //     $row = $sql->detailSearch($account);
-
-    //     $this->assertEquals($expect, $row);
-    // }
+        $this->assertEquals($expect, $row);
+    }
 }
