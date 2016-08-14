233a4391ddb10bc4c5e18fb0ff166ad6eff26f33
hi <demichuang40207@gmail.com>
2016-08-14 02:49:08 +0000

    測試檢查是否有此使用者ok

--- Test/SqlCommandTest.php
+++ Test/SqlCommandTest.php
@@ -4,6 +4,17 @@ require_once("models/SqlCommand.php");
 
 class SqlCommandTest extends \PHPUnit_Framework_TestCase {
 
+	// 測試檢查是否有此使用者
+    public function testCheckUser()
+    {
+    	$account = "jj";
+
+        $sql = new SqlCommand();
+        $num = $sql->checkUser($account);
+
+        $this->assertEquals(1, $num);
+    }
+
 	// 測試查詢餘額
     public function testMoneySearch()
     {
